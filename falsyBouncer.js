70bcda38f9a28b0e5fb2d4f860bc2e76041982e0
Ivan Shvets <37488015+Leocete@users.noreply.github.com>
2018-10-29 22:24:16 +0200

    Update falsyBouncer.js

--- falsyBouncer.js
+++ falsyBouncer.js
@@ -1,3 +1,10 @@
+/* TASK:
+
+Remove all falsy values from an array. 
+Falsy values in JavaScript are false, null, 0, "", undefined, and NaN.
+
+*/
+
 function bouncer(arr) {
   return arr.filter(function(value){
       return value !== false && value !== "" && !(Number.isNaN(value)) && value !== 
